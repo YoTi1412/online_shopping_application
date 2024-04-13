490e4a961a756bd583f53dc28a51d3de8ba4eda1
YoTi <yasserrafai5@gmail.com>
2024-04-13 18:43:05 +0000

    service discovery using netflix eureka

---
+++ online_shopping_application_new/order-service/src/main/java/org/yoti/order_service/service/OrderService.java
@@ -0,0 +1,68 @@
+package org.yoti.order_service.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.yoti.order_service.dto.InventoryResponse;
+import org.yoti.order_service.dto.OrderLineItemsDto;
+import org.yoti.order_service.dto.OrderRequest;
+import org.yoti.order_service.model.Order;
+import org.yoti.order_service.model.OrderLineItems;
+import org.yoti.order_service.repository.OrderRepository;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemsList(orderLineItems);
+
+
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory service and place the order if is the product in stock
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        assert inventoryResponseArray != null;
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isIaInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again");
+        }
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        return orderLineItems;
+    }
+}
